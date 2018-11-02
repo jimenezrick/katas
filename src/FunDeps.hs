daf7f048e65d7667fe39124dbfc9eb233891aecb
Ricardo Catalinas Jiménez <r@untroubled.be>
2018-11-02 22:50:37 +0000

    Has: done

---
+++ src/FunDeps.hs
@@ -0,0 +1,4 @@
+--
+-- https://ocharles.org.uk/posts/2014-12-14-functional-dependencies.html
+--
+module FunDeps where
--- src/Has.hs
+++ src/Has.hs
@@ -1,25 +1,62 @@
+{-# LANGUAGE FlexibleInstances #-}
+
+--
+-- https://www.reddit.com/r/haskell/comments/6jy8yu/the_has_type_class_pattern/
+--
 module Has where
 
-import Data.Set
+import Control.Monad.Reader
+import Data.Set (Set)
+import Data.Set as S
 
-data Thing = Thing
+data Thing =
+    Thing
     deriving (Show, Eq, Ord)
 
 class HasThings a where
-  getThings :: a -> Set Thing
+    getThings :: a -> Set Thing
 
-newtype ThisThings = ThisThings { getThisThings :: [Thing] }
-    deriving Show
+newtype ThisThings = ThisThings
+    { getThisThings :: [Thing]
+    } deriving (Show)
 
 data OtherThing = OtherThing
     { one :: Maybe Thing
     , other :: Thing
-    } deriving Show
+    } deriving (Show)
 
-
-
-
-
+instance HasThings Thing where
+    getThings = S.singleton
 
 instance HasThings ThisThings where
-  getThings = fromList . getThisThings
+    getThings = S.fromList . getThisThings
+
+this :: (MonadReader r m, HasThisEnv r) => m Bool
+this = do
+    e <- asks getThisEnv
+    return (e == 0)
+
+that :: (MonadReader r m, HasThatEnv r) => m Int
+that = do
+    e <- asks getThatEnv
+    return (read e)
+
+class HasThisEnv a where
+    getThisEnv :: a -> Int
+
+class HasThatEnv a where
+    getThatEnv :: a -> String
+
+instance HasThisEnv (Int, String) where
+    getThisEnv = fst
+
+instance HasThatEnv (Int, String) where
+    getThatEnv = snd
+
+thisAndThat :: Reader (Int, String) Int
+thisAndThat = do
+    b <- this
+    n <- that
+    if b
+        then return n
+        else return (n + 1)
