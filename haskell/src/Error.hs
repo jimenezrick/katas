71f8c37b61652ec9fbee9b6e41ba151d40c7dd5c
Ricardo Catalinas Jiménez <r@untroubled.be>
2019-01-27 17:21:39 +0000

    MonadError

--- haskell/src/Error.hs
+++ haskell/src/Error.hs
@@ -1,11 +1,14 @@
+{-# LANGUAGE FlexibleContexts #-}
+
 module Error where
 
 import Control.Monad.Except
+import Control.Monad.IO.Class
 
 test :: IO (Either String Int)
 test = runExceptT fail1
 
-fail1 :: ExceptT String IO Int
+fail1 :: (MonadError String m, MonadIO m) => m Int
 fail1 = do
     liftEither good
     throwError "fuck"
