4ba6e1957b0f4121bae018e89362659a9e8e9841
Ricardo Catalinas Jiménez <r@untroubled.be>
2019-12-08 23:30:38 +0000

    Optimize MaxMinCounts with a Set

--- haskell/src/MaxMinCounts.hs
+++ haskell/src/MaxMinCounts.hs
@@ -1,9 +1,14 @@
+{-# LANGUAGE TupleSections #-}
+
 module MaxMinCounts where
 
 import Control.Monad
 import Control.Monad.Primitive
 import Criterion
 import Criterion.Main
+import Data.List (foldl')
+import Data.Set (Set)
+import qualified Data.Set as S
 import qualified Data.Vector as V
 import Data.Vector (Vector)
 import qualified Data.Vector.Algorithms.Insertion as VA
@@ -11,56 +16,78 @@ import qualified Data.Vector.Unboxed as VU
 import qualified Data.Vector.Unboxed.Mutable as MV
 import Data.Vector.Unboxed.Mutable (MVector)
 
-type ReplicasMtx s = Vector (MVector s Int)
+type MinMaxCounts s = (Int, Int, Vector (MVector s Int), Set (Int, Int))
 
-initReplicasMtx :: PrimMonad m => Int -> Int -> m (ReplicasMtx (PrimState m))
-initReplicasMtx shards replicas = V.replicateM shards $ MV.replicate replicas 2
+initMinMaxCounts :: PrimMonad m => Int -> Int -> Int -> Int -> m (MinMaxCounts (PrimState m))
+initMinMaxCounts initVal mc shards replicas = do
+  mx <- V.replicateM shards $ MV.replicate replicas initVal
+  l <- V.imapM (\i -> ((,i) <$>) . (MV.clone >=> countMax mc)) mx
+  let s = S.fromList $ V.toList l
+  return (mc, fst $ S.findMin s, mx, s)
 
-printReplicasMtx :: ReplicasMtx (PrimState IO) -> IO ()
-printReplicasMtx =
-  V.imapM_ (\i row -> putStr (show i ++ ": ") >> VU.freeze row >>= printRow >> putStrLn "")
+minMaxCount :: PrimMonad m => MinMaxCounts (PrimState m) -> m Int
+minMaxCount (_, min', _, _) = return min'
+
+minCount :: PrimMonad m => MinMaxCounts (PrimState m) -> m Int
+minCount (mc, _, _, _) = return mc
+
+updateMinMaxCounts :: PrimMonad m => [(Int, Int, Int)] -> MinMaxCounts (PrimState m) -> m (MinMaxCounts (PrimState m))
+updateMinMaxCounts changes (mc, _, mx, maxSet) =
+  do
+    setChanges <-
+      forM changes $
+        \(shard, replica, new) -> do
+          oldCount <- MV.clone (mx V.! shard) >>= countMax mc
+          writeMx shard replica new
+          newCount <- MV.clone (mx V.! shard) >>= countMax mc
+          return (shard, (oldCount, newCount))
+    let newMaxSet = updateMaxSet setChanges maxSet
+    return (mc, fst $ S.findMin newMaxSet, mx, newMaxSet)
+  where
+    writeMx shard replica val = MV.write (mx V.! shard) replica val
+
+updateMaxSet :: [(Int, (Int, Int))] -> Set (Int, Int) -> Set (Int, Int)
+updateMaxSet changes = insertions . removals
+  where
+    removals = flip (foldl' (\s (shard, (oldCount, _)) -> S.delete (oldCount, shard) s)) changes
+    insertions = flip (foldl' (\s (shard, (_, newCount)) -> S.insert (newCount, shard) s)) changes
+
+printMinMaxCounts :: MinMaxCounts (PrimState IO) -> IO ()
+printMinMaxCounts (mc, min', mx, maxSet) = do
+  putStrLn $ "minCount: " ++ show mc
+  putStrLn $ "min: " ++ show min'
+  putStrLn $ "maxSet: " ++ show maxSet
+  putStrLn "matrix:"
+  V.imapM_ (\i row -> putStr ("(" ++ show i ++ ") ") >> VU.freeze row >>= printRow >> putStrLn "") mx
   where
     printRow = VU.mapM_ (\x -> putStr $ show x ++ " ")
 
--- | >>> maxReplicaOverlap 1 $ V.empty
--- 0
---
--- >>> maxReplicaOverlap 2 $ V.fromList [3, 1, 2]
--- 2
-maxReplicaOverlap :: PrimMonad m => Int -> MVector (PrimState m) Int -> m Int
-maxReplicaOverlap minReplicas v
-  | MV.length v < minReplicas = return 0
+countMax :: PrimMonad m => Int -> MVector (PrimState m) Int -> m Int
+countMax mc v
+  | MV.length v < mc = return 0
   | otherwise = do
     VA.sort v
-    MV.read v $ MV.length v - minReplicas
+    MV.read v $ MV.length v - mc
 
 benchmark :: IO ()
 benchmark = do
-  v1 <- VU.thaw $ VU.fromList [5, 7, 3, 1, 4, 2, 1, 3, 3, 3, 4]
-  --
-  mtx <- initReplicasMtx (500 * 8) 16
-  MV.write (mtx V.! 203) 8 1
-  MV.write (mtx V.! 203) 1 1
-  MV.write (mtx V.! 203) 5 1
-  --
+  mmc <- initMinMaxCounts 2 14 (500 * 8) 16
   defaultMain
-    [ bench "maxReplicaOverlap" $ nfIO (maxReplicaOverlap 3 v1),
-      bench "computeMaxOverlap" $ nfIO (computeMaxOverlap 14 mtx)
+    [ bench "updateMinMaxCounts" $ nfIO (updateMinMaxCounts changes mmc >>= updateMinMaxCounts undos)
     ]
-
-computeMaxOverlap :: PrimMonad m => Int -> ReplicasMtx (PrimState m) -> m Int
-computeMaxOverlap minReplicas mtx =
-  V.minimum <$> V.mapM (MV.clone >=> maxReplicaOverlap minReplicas) mtx
+  where
+    changes = [(203, 8, 1), (203, 1, 1), (203, 5, 1)]
+    undos = [(203, 8, 2), (203, 1, 2), (203, 5, 2)]
 
 test :: IO ()
 test = do
-  mtx <- initReplicasMtx (500 * 8) 16
-  computeMaxOverlap 14 mtx >>= print -- 2
-  MV.write (mtx V.! 203) 8 1
-  computeMaxOverlap 14 mtx >>= print -- 2
-  MV.write (mtx V.! 203) 1 1
-  computeMaxOverlap 14 mtx >>= print -- 2
-  MV.write (mtx V.! 203) 5 1
-  computeMaxOverlap 14 mtx >>= print -- 1
-  MV.write (mtx V.! 203) 5 3
-  computeMaxOverlap 14 mtx >>= print -- 2
+  mmc <- initMinMaxCounts 2 14 (500 * 8) 16
+  minMaxCount mmc >>= print -- 2
+  mmc <- updateMinMaxCounts [(203, 8, 1)] mmc
+  minMaxCount mmc >>= print -- 2
+  mmc <- updateMinMaxCounts [(203, 1, 1)] mmc
+  minMaxCount mmc >>= print -- 2
+  mmc <- updateMinMaxCounts [(203, 5, 1)] mmc
+  minMaxCount mmc >>= print -- 1
+  mmc <- updateMinMaxCounts [(203, 5, 3)] mmc
+  minMaxCount mmc >>= print -- 2
